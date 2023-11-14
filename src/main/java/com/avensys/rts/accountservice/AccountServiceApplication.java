cbf29c34927cae598a37a7a00798e4a91001294c
rku-avensys <rahul@aven-sys.com>
2023-11-14 09:16:21 +0530

    changes

--- src/main/java/com/avensys/rts/accountservice/AccountServiceApplication.java
+++ src/main/java/com/avensys/rts/accountservice/AccountServiceApplication.java
@@ -1,11 +1,9 @@
 package com.avensys.rts.accountservice;
 
-import com.avensys.rts.accountservice.interceptor.AuditInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
 @SpringBootApplication
 @EnableFeignClients
