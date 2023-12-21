1ecc33894b703a90360084f44d9fa58418307d40
rku-avensys <rahul@aven-sys.com>
2023-12-21 16:46:14 +0530

    appconfig fixes

--- src/main/java/com/avensys/rts/accountservice/config/AppConfig.java
+++ src/main/java/com/avensys/rts/accountservice/config/AppConfig.java
@@ -1,62 +1,58 @@
 package com.avensys.rts.accountservice.config;
 
-import com.avensys.rts.accountservice.interceptor.AuditInterceptor;
-import com.avensys.rts.accountservice.interceptor.AuthInterceptor;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.io.IOException;
+import com.avensys.rts.accountservice.interceptor.AuditInterceptor;
+import com.avensys.rts.accountservice.interceptor.AuthInterceptor;
 
 /**
- * @author Koh He Xiang
- * This class is used to configure the application.
+ * @author Koh He Xiang This class is used to configure the application.
  */
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
 
-    @Autowired
-    private AuthInterceptor authInterceptor;
+	@Autowired
+	private AuthInterceptor authInterceptor;
 
-    /**
-     * This method is used to register the interceptors.
-     * @param registry
-     */
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AuditInterceptor());
-        registry.addInterceptor(authInterceptor);
-    }
+	/**
+	 * This method is used to register the interceptors.
+	 * 
+	 * @param registry
+	 */
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new AuditInterceptor());
+		registry.addInterceptor(authInterceptor);
+	}
 
-    /**
-     * This method is used to configure the message source for internationalization.
-     * These messages are used to display error messages to the user.
-     * These messages are used to send back messages in response
-     * @return
-     */
-    @Bean
-    public MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource ();
-        messageSource.setBasenames("messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        return messageSource;
-    }
+	/**
+	 * This method is used to configure the message source for internationalization.
+	 * These messages are used to display error messages to the user. These messages
+	 * are used to send back messages in response
+	 * 
+	 * @return
+	 */
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasenames("messages");
+		messageSource.setDefaultLocale(Locale.ENGLISH);
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
 
-    @Bean
-    public RestTemplate restTemplate() {
-    	return new RestTemplate();
-    }
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 }
